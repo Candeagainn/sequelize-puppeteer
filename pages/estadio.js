e99748cc84eef0e8daefe627d8c893ad7f4fed92
candeagain <candeagainn@gmail.com>
2024-06-26 18:39:11 -0300

    deleted unnecesary comments

--- pages/estadio.js
+++ pages/estadio.js
@@ -35,20 +35,3 @@ class EstadioScraper extends BaseScraper {
 
 export default EstadioScraper;
     
-    
-
-//     const result = await page.evaluate(()=> {
-//         const lista = document.querySelectorAll('.right');
-//         const nombres = [...lista].map(e => e.querySelector('h4 > a').innerText);
-//         const capacidad = [...lista].map(e => e.querySelector('.details > dd:nth-child(4)').innerText);
-//         console.log([nombres])
-//         return{
-//             nombres,
-//             capacidad
-//         }
-//     })
-//     console.log(result);
-//     await browser.close();
-// }
-
-// openWebPage();
